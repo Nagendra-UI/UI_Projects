61fed4c0ad715580ba852fe4473a13c2a5cca00a
Nagendra-UI <nagendra.bmail@gmail.com>
2021-03-17 20:52:07 +0530

    3 new ui projects

---
+++ Text-Wave-Effect/script.js
@@ -0,0 +1,10 @@
+const labels = document.querySelectorAll('.form-control label');
+
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText.split("").map((letter, id) => {
+        return `<span style="transition-delay: ${id * 50}ms">${letter}</span>`
+    }).join("")
+
+    console.log(label.innerHTML)
+})
